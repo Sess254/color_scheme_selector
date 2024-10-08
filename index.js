da5388e51d64d8385683febce744e040ec89a67f
Sess254 <wamaritemoses@gmail.com>
2024-10-08 15:23:59 +0300

    main functions works,needs work in styling and refactoring

---
+++ index.js
@@ -0,0 +1,40 @@
+const btnEl = document.getElementById('btn-el')
+const colorEl = document.getElementById('color-el')
+const colorSelection = document.getElementById('color-selector')
+const colorModeSelection = document.getElementById('mode-selector')
+
+let selectedMode = ""
+let selectedColor = ""
+
+colorSelection.addEventListener('change', (e) => {
+    const colorValue = colorSelection.value
+    selectedColor = colorValue.replace("#", "")
+
+})
+
+colorModeSelection.addEventListener('change', (e) => {
+    selectedMode = colorModeSelection.value
+    console.log(selectedMode)
+})
+
+
+
+
+btnEl.addEventListener('click', () => {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}`)
+        .then(res => res.json())
+        .then(data => {
+            colorEl.innerHTML = ""
+
+            for (let color of data.colors){
+                console.log(color)
+                colorEl.innerHTML += `
+                        <div class="colors" id="color-el">
+                            <img src="${color.image.bare}">
+                            <p>${color.hex.value}</p>
+                        </div> 
+                    `
+            }
+
+        })
+})
